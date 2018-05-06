afc39bc76c6f4f8b99bdb242145deecbc3f39b88
Matt Duffield <matt.duffield@gmail.com>
2018-05-06 12:23:04 -0700

    Auto commit from FrontEnd Creator

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+// import map from 'lodash/map';
 
-function init() {
-	// read the package.json file
-	console.log('attempting to read package.json');
-	axios.get('package.json').then(response => {
-		const {status, data} = response;
-		console.log('package.json', data);
-	});
+var nums = [1, 2, 3];
+
+function square(x) {
+    return x*x;
 }
 
-init();
+console.log('square', square(3));
+// console.log(map(nums, square));
