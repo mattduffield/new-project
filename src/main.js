0f69169d373444b7902e308433e0acb9be06fbb4
Matt Duffield <matt.duffield@gmail.com>
2018-05-07 21:47:46 -0700

    Auto commit from FrontEnd Creator

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import {PLATFORM} from 'aurelia-pal';
 import 'babel-polyfill';
 import * as Bluebird from 'bluebird';
-import 'bootstrap/dist/css/bootstrap.css';
 
 export async function configure(aurelia) {
   aurelia.use
