5789dc9223862de7a8fbe33ee0167e48b9a46c68
Matt Duffield <matt.duffield@gmail.com>
2018-05-08 08:25:57 -0700

    Auto commit from FrontEnd Creator

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,16 +1,12 @@
 (function (global) {
   System.config({
     baseURL: "src",
-    // baseURL: "https://mattduffield.github.io/new-project/",
-    defaultJSExtensions: true, // This has been deprecated in the latest version. Need to get this working properly in latest     
+    defaultJSExtensions: true, 
     transpiler: 'plugin-babel',
     paths: {
-      // paths serve as alias
       'npm:': 'https://unpkg.com/'
     },
-    // map tells the System loader where to look for things
     map: {
-      // SystemJS Plugins
       'plugin-babel':               'npm:systemjs-plugin-babel@0.0.25/plugin-babel.js',
       'systemjs-babel-build':       'npm:systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js',
       'plugin-css':                 'npm:systemjs-plugin-css@0.1.37/css.js',
@@ -19,8 +15,8 @@
     },
     packages: {
       src: {
-        format: "amd",
-        main: "main.js",
+        // format: "amd",
+        // main: "main.js",
         defaultExtension: 'js',
         meta: {
           '*.css': {
