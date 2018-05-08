08af35aeb8057db92a55ff0c9abad44d5e0d471f
Matt Duffield <matt.duffield@gmail.com>
2018-05-07 23:21:38 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -6,5 +6,19 @@ export class App {
   constructor() {
 
   }
+
+  attached() {
+    compute();
+  }
+  async compute() {
+    const message2 = await computeMessage();
+  }
+  async computeMessage() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve('How do you like me now?');
+      },3000);
+    });
+  }
 }
     
