3d77e074c3a26ae575b5b1e93393e8e081f714b2
Matt Duffield <matt.duffield@gmail.com>
2018-05-08 08:35:24 -0700

    Auto commit from FrontEnd Creator

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-/*
-// import map from 'lodash/map';
-import _ from 'lodash';
-// import compose from 'koa-compose';
-// import {Application as Koa} from './lib/koa-web/application.js';
-
-var nums = [1, 2, 3];
-
-function square(x) {
-  return x*x;
-}
-async function test() {
-  return new Promise((resolve, reject) => {
-    resolve('hello');
-  });
-}
-async function doWork() {
-  const msg = await test();
-  console.log('message', msg);
-}
-console.log('square', square(3));
-// console.log(map(nums, square));
-console.log(_.map(nums, square));
-// console.log('Koa', Koa);
-// console.log('compose', compose);
-// doWork();
-*/
-
 function hashCode(s) {
   if (!s) return 0;
   var h = 0, l = s.length, i = 0;
--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
+/*
+// import map from 'lodash/map';
+import _ from 'lodash';
+// import compose from 'koa-compose';
+// import {Application as Koa} from './lib/koa-web/application.js';
+
+var nums = [1, 2, 3];
+
+function square(x) {
+  return x*x;
+}
+async function test() {
+  return new Promise((resolve, reject) => {
+    resolve('hello');
+  });
+}
+async function doWork() {
+  const msg = await test();
+  console.log('message', msg);
+}
+console.log('square', square(3));
+// console.log(map(nums, square));
+console.log(_.map(nums, square));
+// console.log('Koa', Koa);
+// console.log('compose', compose);
+// doWork();
+*/
+
 import {AppService} from 'services/app-service';
 
 
