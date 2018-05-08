43f38e6a3a644b74b82571b22650c61fe1d259c7
Matt Duffield <matt.duffield@gmail.com>
2018-05-07 20:20:01 -0700

    Auto commit from FrontEnd Creator

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import map from 'lodash/map';
 import _ from 'lodash';
-import {Application as Koa} from './lib/koa-web/application';
+import {Application as Koa} from './lib/koa-web/application.js';
 
 var nums = [1, 2, 3];
 
