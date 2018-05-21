aa35e3a22cad1494426afa1b48768da0df458220
Matt Duffield <matt.duffield@gmail.com>
2018-05-20 21:58:03 -0700

    Auto commit from FrontEnd Creator

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,6 +10,7 @@
       'plugin-babel':               'npm:systemjs-plugin-babel@0.0.25/plugin-babel.js',
       'systemjs-babel-build':       'npm:systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js',
       'plugin-css':                 'npm:systemjs-plugin-css@0.1.37/css.js',
+      'plugin-json':                 'npm:systemjs-plugin-json@0.3.0/json.js',
       'babel-polyfill':             'npm:babel-polyfill@^6.26.0/dist/polyfill.min.js',
       'bluebird':                   'npm:bluebird@3.5.1/js/browser/bluebird.min.js'
     },
@@ -19,6 +20,9 @@
         meta: {
           '*.css': {
             loader: 'plugin-css'
+          },
+          '*.json': { 
+            loader: 'plugin-json' 
           },
         }
       }
