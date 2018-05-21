59d5b94bc16a49f00975246a1203d6bcfe8458b9
Matt Duffield <matt.duffield@gmail.com>
2018-05-20 22:19:33 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -2,26 +2,26 @@ import {AppService} from 'services/app-service';
 import {Koa as Application} from '../lib/koa-web/application';
 
 export class App {
-  static inject = [AppService];
-  // static inject = [AppService, Koa];
+  // static inject = [AppService];
+  static inject = [AppService, Koa];
 
-  constructor(appSvc) {
-  // constructor(appSvc, Koa) {
+  // constructor(appSvc) {
+  constructor(appSvc, Koa) {
     this.appSvc = appSvc;
-    // this.Koa = Koa;
+    this.Koa = Koa;
   }
 
   attached() {
     this.appSvc.setupConsole();
     this.appSvc.displayInit();
 
-    // console.log('starting Koa server...');
-    // this.Koa.use(async function(ctx) {
-    //   ctx.body = 'hello world';
-    //   console.log('started Koa server...');
-    // });
-    // this.Koa.listen(8080);
-    // console.log('Koa server listening on port 8080...');
+    console.log('starting Koa server...');
+    this.Koa.use(async function(ctx) {
+      ctx.body = 'hello world';
+      console.log('started Koa server...');
+    });
+    this.Koa.listen(8080);
+    console.log('Koa server listening on port 8080...');
   }
   
 }
