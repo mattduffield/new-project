09c1d4dd223753e323a9c92fd48bb348a602c6b2
Matt Duffield <matt.duffield@gmail.com>
2018-05-20 21:46:44 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import {AppService} from 'services/app-service';
+import {Koa as Application} from '../../lib/koa-web/application';
 
 // changes here...
 export class App {
-  static inject = [AppService];
+  static inject = [AppService, Koa];
 
-  constructor(appSvc) {
+  constructor(appSvc, Koa) {
     this.appSvc = appSvc;
+    this.Koa = Koa;
   }
 
   attached() {
